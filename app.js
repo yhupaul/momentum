03312d05c010db29f8f648e3bb6ae5308ea6dbc5
yhupaul <yhupaul@hotmail.com>
2021-11-13 22:01:45 -0800

    review function calculating

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
 function sayHello(nameOfPerson, age) {
-  console.log("Hello my name is " + nameOfPerson + " and I'm " + age)
+  console.log("Hello my name is " + nameOfPerson + " and I'm " + age);
 }
 
 sayHello("nico", 10);
 sayHello("dan", 21);
 sayHello("lynn", 25);
+
+function plus(a, b) {
+  console.log(a + b);
+}
+
+function divide(firstNumber, secondNumber) {
+  console.log(firstNumber / secondNumber);
+}
+
+plus(60, 8);
+divide(120, 2);
