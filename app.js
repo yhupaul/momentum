a09b928aa69d3c8d018923d90449720f4131cbbb
yhupaul <yhupaul@hotmail.com>
2021-11-14 11:42:51 -0800

    review conditional part

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
-const calculator = {
-  add: function(a, b) {
-    console.log(a + b);
-  },
-  minus: function(a, b) {
-    console.log(a - b);
-  },
-  times: function(a, b) {
-    console.log(a * b);
-  },
-  divide: function(a, b) {
-    console.log(a / b);
-  },
-  power: function(a, b) {
-    console.log(a ** b);
-  },
-}
+const age = parseInt(prompt("How old are you?"));
 
-calculator.add(2, 4);
-calculator.minus(6, 3);
-calculator.times(10, 5);
-calculator.divide(100, 24);
-calculator.power(2, 2);
+// true || true === true
+// true || false === true
+// false || true === true
+// false || false === false
+
+// true && true === true
+// true && false === false
+// false && true === false
+// false && false === false
+ 
+if (isNaN(age) || age < 0) {
+  console.log("Please write a real positive number");
+} else if (age < 18) {
+  console.log("You are too young.");
+} else if (age >= 18 && age <= 50) {
+  console.log("You can drink");
+} else if (age > 50 && age <= 80) {
+  console.log("You should exercise");
+} else if (age > 80) {
+  console.log("You can do whatever you want.")
+}
