c9d122ff4c8732b7b640bfc86034cee1b851b0d9
yhupaul <yhupaul@hotmail.com>
2021-11-13 22:04:54 -0800

    review function inside of Object

--- app.js
+++ app.js
@@ -1,18 +1,28 @@
-function sayHello(nameOfPerson, age) {
-  console.log("Hello my name is " + nameOfPerson + " and I'm " + age);
-}
+// function sayHello(nameOfPerson, age) {
+//   console.log("Hello my name is " + nameOfPerson + " and I'm " + age);
+// }
 
-sayHello("nico", 10);
-sayHello("dan", 21);
-sayHello("lynn", 25);
+// sayHello("nico", 10);
+// sayHello("dan", 21);
+// sayHello("lynn", 25);
 
-function plus(a, b) {
-  console.log(a + b);
-}
+// function plus(a, b) {
+//   console.log(a + b);
+// }
 
-function divide(firstNumber, secondNumber) {
-  console.log(firstNumber / secondNumber);
-}
+// function divide(firstNumber, secondNumber) {
+//   console.log(firstNumber / secondNumber);
+// }
 
-plus(60, 8);
-divide(120, 2);
+// plus(60, 8);
+// divide(120, 2);
+
+const player = {
+  name: "dan",
+  sayHello: function (otherPersonsName) {
+    console.log("hello " + otherPersonsName + " nice to meet you")
+  },
+};
+
+player.sayHello("lynn");
+player.sayHello("nico");
