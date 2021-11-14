ff8b1ce6f06db33a0298c00c765680dbf49e212d
yhupaul <yhupaul@hotmail.com>
2021-11-13 19:20:04 -0800

    review const and let

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-alert("hi");
+const a = 5;
+const b = 2;
+let myName = "Dan";
+
+console.log(a + b);
+console.log(a * b);
+console.log(a / b);
+console.log("hello " + myName);
+
+myName = "Hwanuk";
+
+console.log("your new name is " + myName);
+
