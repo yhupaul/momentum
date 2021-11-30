3d629f1e40082a6eb80123c82d3d6477511f2006
yhupaul <yhupaul@hotmail.com>
2021-11-29 17:22:19 -0800

    concept of how to get elements from HTML into JS

--- app.js
+++ app.js
@@ -1,23 +1,7 @@
-const age = parseInt(prompt("How old are you?"));
+const title = document.getElementById("title");
 
-// true || true === true
-// true || false === true
-// false || true === true
-// false || false === false
+title.innerText = "Got you!";
 
-// true && true === true
-// true && false === false
-// false && true === false
-// false && false === false
- 
-if (isNaN(age) || age < 0) {
-  console.log("Please write a real positive number");
-} else if (age < 18) {
-  console.log("You are too young.");
-} else if (age >= 18 && age <= 50) {
-  console.log("You can drink");
-} else if (age > 50 && age <= 80) {
-  console.log("You should exercise");
-} else if (age > 80) {
-  console.log("You can do whatever you want.")
-}
+console.log(title.id);
+console.log(title.className);
+
