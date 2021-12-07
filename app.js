eab0c13361ec7669a2a5522d75489d3a16455082
yhupaul <yhupaul@hotmail.com>
2021-12-06 16:51:58 -0800

    learn abour Form in HTML Submission

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const loginInput = document.querySelector("#login-form input");
 const loginButton = document.querySelector("#login-form button");
 
 function onLoginBtnClick() {
-  console.log(loginInput.value);
-}
+  const username = loginInput.value;
+  console.log(username);
 
 loginButton.addEventListener("click", onLoginBtnClick);
