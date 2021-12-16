433e93971e6edc6bb23ffcfeb3d088b08f1901e3
yhupaul <yhupaul@hotmail.com>
2021-12-15 19:59:30 -0800

    save todolist

--- js/todo.js
+++ js/todo.js
@@ -2,6 +2,12 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
 
+const toDos = [];
+
+function saveToDo() {
+  localStorage.setItem("todos", JSON.stringify(toDos));
+}
+
 function deleteToDo(event) {
   const li = event.target.parentElement;
   li.remove();
@@ -23,7 +29,9 @@ function handleToDoSubmit(event) {
   event.preventDefault(event);
   const newTodo = toDoInput.value;
   toDoInput.value = "";
+  toDos.push(newTodo);
   paintToDo(newTodo);
+  saveToDo();
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
